bde44f754903b7f4bccd51b95e26d78fbaa2f52a
sinopino391 <sinopino94@gmail.com>
2023-09-13 18:54:42 +0200

    commit msg

--- src/components/githubmodal.js
+++ src/components/githubmodal.js
@@ -1,10 +1,8 @@
-'use client';
-import React, {useState, useEffect} from 'react';
-import { supabase } from '@/lib/supabase';
+"use client";
+import React, { useState, useEffect } from "react";
+import { supabase } from "@/lib/supabase";
 
 const Modal = ({ isOpen, onClose }) => {
-  if (!isOpen) return null;
-
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -18,7 +16,7 @@ const Modal = ({ isOpen, onClose }) => {
 
   async function signInWithGitHub() {
     const { data, error } = await supabase.auth.signInWithOAuth({
-      provider: 'github',
+      provider: "github",
     });
     setUser(data.user);
   }
@@ -28,32 +26,39 @@ const Modal = ({ isOpen, onClose }) => {
     setUser(null);
   }
 
+  if (!isOpen) return null;
+
   return (
     <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-20 bg-gray-300 bg-opacity-40 backdrop-filter backdrop-blur-md">
       {/* Hintergrund mit Transparenz */}
-      <div className="modal-bg fixed inset-0 bg-gray-800 opacity-50"
-        onClick={onClose}></div>
+      <div
+        className="modal-bg fixed inset-0 bg-gray-800 opacity-50"
+        onClick={onClose}
+      ></div>
       {/* Modal-Container mit erhöhter Mindesthöhe */}
       <div className="modal-content bg-white p-4 rounded shadow-md relative text-center">
         {user != null ? (
           <>
-          <h2 className="text-xl font-semibold mb-4">
-            Eingeloggt als, {user.email}
-          </h2>
-          <button  className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 z-2"
-          onClick={signOut}>
-            Ausloggen
-          </button></>
-          ) : (
-        <>
-        <h2 className="text-xl font-semibold mb-4">Anmelden mit GitHub</h2>
-        <button
-          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 z-2"
-          onClick={signInWithGitHub}
-        >
-          GitHub anmelden
-        </button>
-        </>
+            <h2 className="text-xl font-semibold mb-4">
+              Eingeloggt als, {user.email}
+            </h2>
+            <button
+              className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 z-2"
+              onClick={signOut}
+            >
+              Ausloggen
+            </button>
+          </>
+        ) : (
+          <>
+            <h2 className="text-xl font-semibold mb-4">Anmelden mit GitHub</h2>
+            <button
+              className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 z-2"
+              onClick={signInWithGitHub}
+            >
+              GitHub anmelden
+            </button>
+          </>
         )}
       </div>
     </div>
