9e8f7e2258e80bd6a697c4fc5110d090a3d875e6
Enes Poyraz <epoyraz.eth@googlemail.com>
2023-09-22 03:55:10 +0200

    add Pricing

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Pricing from "@/components/pricing";
+
+export default function Home() {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center">
+        <Pricing />
+      </main>
+    </>
+  );
+}
